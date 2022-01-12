e2452da6d2841aa1e2e67ca261821916eb2df2f8
sk <magicbaby810@gmail.com>
2022-01-12 22:39:06 +0800

    调整配置

--- testgradle/src/main/groovy/com/sk/testplugin/TestPlugin.groovy
+++ testgradle/src/main/groovy/com/sk/testplugin/TestPlugin.groovy
@@ -1,10 +1,18 @@
+package com.sk.testplugin
+
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import com.android.build.gradle.BaseExtension
 
 class TestPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println("hello world! evve body")
+        if (Class.forName("com.android.build.gradle.BaseExtension")) {
+            println("hello world! evve body")
+            BaseExtension android = project.extensions.getByType(BaseExtension)
+        } else {
+            println("hello world! evve body")
+        }
     }
 }
